2b31f057845246fef002f709cf6fc8e2d0166ba4
d-chandrahas <chandrahasreddy732@gmail.com>
2024-02-25 04:17:19 +0530

    finished login request

--- attendance.py
+++ attendance.py
@@ -3,14 +3,15 @@ import requests as req
 import json
 
 
-# BASE_URL = "https://erp.iith.ac.in/MobileAPI/"
-BASE_URL = "http://127.0.0.1:5000/MobileAPI/"
+BASE_URL = "https://erp.iith.ac.in/MobileAPI/"
 
 LOGIN_PATH = "GetMobileAppValidatePassword"
 
 CONFIG_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.json")
 
 CONFIG = {}
+
+FAKE_FCMID = "WQaNTlvZNsk22L-3LmDu7Y:W4cKL23YQsnIkyfgk4QQ5uGotc-U6K40QO6ydXeou8eGLHtj3VZRJ2ixbJSYcqbN59vnz7TvVVpYfa5i95o0X2aa8kxv0lQ1RyBkpsqmz9YNWRpNh3fJ5u0t7ZtuX4Mm9FKdQGdt2V2W,OS:33,Model:SM-M325F,BRAND:samsung,MANUFACTURER:samsung,Build ID:RSR1.210722.013"
 
 
 def cls():
@@ -23,7 +24,7 @@ def load_config():
         with open(CONFIG_PATH, "r") as f:
             CONFIG = json.load(f)
     else:
-        CONFIG["Webidentifier"] = None
+        CONFIG["WebIdentifier"] = None
         with open(CONFIG_PATH, "w") as f:
             json.dump(CONFIG, f)
 
@@ -35,21 +36,32 @@ def modify_config(key, value):
 
 
 def login_req(userid, password):
-    body = { # fixme all keys
+    body = {
         "UserID": userid,
+        "DeviceType": "android",
+        "FCMID" : FAKE_FCMID,
         "Password": password
     }
-    # ! headers
-    res = req.post(BASE_URL + LOGIN_PATH, json=body) # fixme body text spacing
 
+    try:
+        res = req.post(BASE_URL + LOGIN_PATH, json=body)
+    except Exception as e:
+        return False, None, f"Error: {e}\nProbably a server-side error, IDK tho"
+    
+    data = res.json()[0]
     if res.status_code == 200:
-        success = True
-        webid = res.json()["Webidentifier"] # fixme proper json key
-        error_msg = None
+        if data["errorId"] == 0:
+            success = True
+            webid = data["referenceId"]
+            error_msg = None
+        else:
+            success = False
+            webid = None
+            error_msg = data["errorMessage"]
     else:
         success = False
         webid = None
-        error_msg = res.text # fixme proper json key
+        error_msg = f"Http Status {res.status_code}\n{res.text}"
 
     return success, webid, error_msg
 
@@ -64,14 +76,14 @@ def login_page():
     success, webid, error_msg = login_req(userid, password)
 
     if success:
-        modify_config("Webidentifier", webid)
+        modify_config("WebIdentifier", webid)
     else:
         print(f"\nError: {error_msg}")
         input("\nPress Enter to retry (or) Ctrl+C to exit")
 
 
 def logout():
-    modify_config("Webidentifier", None)
+    modify_config("WebIdentifier", None)
 
 
 def home_page(): # todo implement
@@ -86,7 +98,7 @@ def main():
     cls()
     load_config()
     while True:
-        while CONFIG["Webidentifier"] is None:
+        while CONFIG["WebIdentifier"] is None:
             login_page()
         home_page()
 
